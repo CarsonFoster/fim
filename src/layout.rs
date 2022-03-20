b2ce6fd5d93f3ac9d622bdd9c0369df007520bdf
CarsonFoster <carsonwfoster@gmail.com>
2022-03-19 21:40:12 -0500

    Added default code to Layout for KeyCode conversions

--- src/layout.rs
+++ src/layout.rs
@@ -1,6 +1,24 @@
+use crossterm::event::KeyCode;
+
 pub trait Layout {
     fn from_qwerty(&self, qwerty_press: u8) -> u8;
     fn to_qwerty(&self, layout_press: u8) -> u8;
+    fn from_qwerty_keycode(&self, qwerty_press: KeyCode) -> KeyCode {
+        match qwerty_press {
+            KeyCode::Char(c) => {
+                if c.len_utf8() > 1 { qwerty_press } else { KeyCode::Char( self.from_qwerty(c as u8) ) }
+            },
+            _ => qwerty_press
+        }
+    }
+    fn to_qwerty_keycode(&self, layout_press: KeyCode) -> KeyCode {
+        match layout_press {
+            KeyCode::Char(c) => {
+                if c.len_utf8() > 1 { layout_press } else { KeyCode::Char( self.to_qwerty(c as u8) ) }
+            },
+            _ => layout_press
+        }
+    }
 }
 
 pub struct Qwerty;
