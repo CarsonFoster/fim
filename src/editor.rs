8f746bacc0c49e57435b4a6fca30a4eee91d81bc
CarsonFoster <carsonwfoster@gmail.com>
2022-03-22 23:12:52 -0500

    Adds support for moving the cursor around the terminal

--- src/editor.rs
+++ src/editor.rs
@@ -19,7 +19,6 @@ impl Editor {
 
     pub fn run(&mut self) -> Result<()> {
         self.setup()?;
-        println!("Hello, world!\r");
         loop {
             self.process_keypress()?; 
             if self.quit {
@@ -30,16 +29,31 @@ impl Editor {
     }
 
     fn setup(&mut self) -> Result<()> {
-        self.terminal.enter_alternate_screen()
+        self.terminal.enter_alternate_screen()?;
+        self.terminal.move_cursor_to(0, 0)?;
+        println!("Hello, world!\r");
+        Ok(())
     }
 
     fn process_keypress(&mut self) -> Result<()> {
         let KeyEvent{ code: c, modifiers: m } = self.terminal.read_key()?;
         match c {
             KeyCode::Char('q') => self.quit = true,
+            KeyCode::Char('h') | KeyCode::Char('j') | KeyCode::Char('k') | KeyCode::Char('l') => self.move_key(c)?,
             _ => ()
         }
         Ok(())
+    }
+
+    fn move_key(&mut self, key: KeyCode) -> Result<()> {
+        match key {
+            KeyCode::Char('h') => self.terminal.cursor_left_by(1),
+            KeyCode::Char('j') => self.terminal.cursor_down_by(1),
+            KeyCode::Char('k') => self.terminal.cursor_up_by(1),
+            KeyCode::Char('l') => self.terminal.cursor_right_by(1),
+            _ => ()
+        }
+        self.terminal.move_cursor()
     }
 }
 
--- src/terminal.rs
+++ src/terminal.rs
@@ -1,11 +1,14 @@
 use std::io::{Stdout, stdout};
 use crossterm::{
+    Result,
+    cursor::{
+        MoveTo,
+    },
     event::{
         Event,
         KeyEvent,
         read,
     },
-    Result,
     execute,
     terminal::{
         self,
@@ -19,9 +22,15 @@ pub struct Size {
     pub height: u16,
 }
 
+pub struct Position {
+    pub x: u16,
+    pub y: u16,
+}
+
 pub struct Terminal {
     size: Size,
     stdout: Stdout,
+    cursor_pos: Position,
 }
 
 impl Terminal {
@@ -29,11 +38,65 @@ impl Terminal {
         let (width, height) = terminal::size()?;
         let stdout = stdout();
         terminal::enable_raw_mode()?;
-        Ok( Terminal{ stdout, size: Size{ width, height } } )
+        Ok( Terminal{ stdout, size: Size{ width, height }, cursor_pos: Position{ x: 0, y: 0 } } )
+    }
+
+    fn saturating_sub(x: u16, d: u16) -> u16 {
+        if d > x {
+            0
+        } else {
+            x - d
+        }
+    }
+
+    fn set_x(&mut self, x: u16) {
+        if x >= 0 && x < self.size.width {
+            self.cursor_pos.x = x;
+        }
+    }
+
+    fn set_y(&mut self, y: u16) {
+        if y >= 0 && y < self.size.height {
+            self.cursor_pos.y = y;
+        }
     }
 
     pub fn size(&self) -> &Size {
         &self.size
+    }
+
+    pub fn cursor_pos(&self) -> &Position {
+        &self.cursor_pos
+    }
+
+    pub fn move_cursor(&mut self) -> Result<()> { 
+        execute!(self.stdout, MoveTo(self.cursor_pos.x, self.cursor_pos.y))
+    }
+
+    pub fn move_cursor_to(&mut self, x: u16, y: u16) -> Result<()> {
+        self.cursor_to(x, y);
+        self.move_cursor()
+    }
+
+    pub fn cursor_to(&mut self, x: u16, y: u16) {
+        self.set_x(x);
+        self.set_y(y);
+    }
+
+    pub fn cursor_left_by(&mut self, d_x: u16) {
+        self.cursor_to(Self::saturating_sub(self.cursor_pos.x, d_x), self.cursor_pos.y);
+    }
+
+    pub fn cursor_right_by(&mut self, d_x: u16) {
+        self.cursor_to(self.cursor_pos.x.saturating_add(d_x), self.cursor_pos.y);
+    }
+
+    pub fn cursor_up_by(&mut self, d_y: u16) {
+        self.cursor_to(self.cursor_pos.x, Self::saturating_sub(self.cursor_pos.y, d_y));
+    }
+
+    pub fn cursor_down_by(&mut self, d_y: u16) {
+        self.cursor_to(self.cursor_pos.x, self.cursor_pos.y.saturating_add(d_y));
     }
 
     pub fn enter_alternate_screen(&mut self) -> Result<()> {
