3a750fddaa76653cbddba97d25dc3ccd57650de1
CarsonFoster <carsonwfoster@gmail.com>
2022-04-23 00:34:35 -0500

    Change Config to a trait, which ConfigFile implements

--- src/config.rs
+++ src/config.rs
@@ -1,9 +1,26 @@
-pub struct Config {
+use crate::editor::Editor;
+use crossterm::{Result, event::KeyEvent};
+
+pub trait Config {
+    fn action(&self, key: KeyEvent) -> Option<dyn FnOnce(&mut Editor) -> Result<()>>;
+}
+
+pub struct ConfigFile {
 
 }
 
-impl Config {
+impl ConfigFile {
     pub fn new(filename: &str) -> Self {
-        Config
+        ConfigFile
+    }
+}
+
+pub struct DefaultConfig;
+
+impl Config for DefaultConfig {
+    fn action(&self, key: KeyEvent) -> Option<dyn FnOnce(&mut Editor) -> Result<()>> {
+        match key {
+             
+        }
     }
 }
