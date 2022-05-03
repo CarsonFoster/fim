ca9cee6b495deda0d2679c071e5a1cb171a8b7d4
CarsonFoster <carsonwfoster@gmail.com>
2022-05-02 21:36:05 -0500

    Add documentation for editor.rs

--- src/editor.rs
+++ src/editor.rs
@@ -1,3 +1,4 @@
+//! A module that contains the main editor logic.
 use crate::context::*;
 use crate::terminal::{Size, Terminal};
 use crossterm::{
@@ -21,21 +22,32 @@ use crossterm::{
 const VERSION: &str = env!("CARGO_PKG_VERSION");
 
 pub struct Editor<'a> {
+    #[doc(hidden)]
     terminal: Terminal,
+    #[doc(hidden)]
     quit: bool,
+    #[doc(hidden)]
     context_stack: Vec<Box<dyn Context + 'a>>,
+    #[doc(hidden)]
     push_context_stack: Vec<Box<dyn Context + 'a>>,
+    #[doc(hidden)]
     has_been_setup_stack: Vec<bool>,
+    #[doc(hidden)]
     command_stack: Vec<String>,
+    #[doc(hidden)]
     welcome_message: [String; 4],
 }
 
 impl<'a> Editor<'a> {
+    /// Create a new Editor struct.
     pub fn new() -> Result<Editor<'a>> {
         let welcome_message = ["FIM - Foster's vIM-like editor".into(), String::new(), format!("Version {}", VERSION), "by Carson Foster".into()];
         Ok( Editor{ terminal: Terminal::new()?, quit: false, welcome_message, context_stack: vec![Box::new(NormalMode)], push_context_stack: Vec::new(), has_been_setup_stack: vec![true], command_stack: Vec::new() } )
     }
 
+    /// Run the editor logic.
+    ///
+    /// Returns only the user has signalled they want to quit.
     pub fn run(&mut self) -> Result<()> {
         self.setup()?;
         loop {
@@ -89,18 +101,29 @@ impl<'a> Editor<'a> {
         Ok(())
     }
 
+    /// Set the quit flag.
     pub fn quit(&mut self) {
         self.quit = true;
     }
 
+    /// Push a [Context](super::context::Context) to the stack of contexts.
     pub fn push_context<C: 'a + Context>(&mut self, context: C) {
         self.push_context_stack.push(Box::new(context)); 
     }
 
+    /// Draw the command line.
+    ///
+    /// (This refers to the last line in the terminal that you type ed commands or ":q" into in
+    /// vim. I have no idea what it's really called, so I refer to it as the command line).
+    /// 
+    /// See also: [`Self::q_draw_cmd_line()`].
     pub fn draw_cmd_line<const N: usize>(&mut self, text: [&str; N]) -> Result<()> {
         self.q_draw_cmd_line(text, true)
     }
 
+    /// Queue the necessary [`crossterm::Command`]s to draw the command line.
+    /// 
+    /// See also: [`Self::draw_cmd_line()`].
     pub fn q_draw_cmd_line<const N: usize>(&mut self, text: [&str; N], flush: bool) -> Result<()> {
         let height = self.terminal.size().height;
         self.terminal.cursor_to(0, height - 1).q_move_cursor()?.q(Clear(ClearType::CurrentLine))?;
@@ -110,14 +133,17 @@ impl<'a> Editor<'a> {
         if flush { self.terminal.flush() } else { Ok(()) }
     }
 
+    /// Return a reference to the terminal.
     pub fn terminal(&mut self) -> &mut Terminal {
         &mut self.terminal
     }
 
+    /// Push a command to the command history stack.
     pub fn push_command(&mut self, cmd: String) {
         self.command_stack.push(cmd);
     }
 
+    /// Return a reference to the command history stack.
     pub fn command_stack(&self) -> &Vec<String> {
         &self.command_stack
     }
@@ -152,6 +178,7 @@ impl<'a> Editor<'a> {
     }
 }
 
+#[doc(hidden)]
 impl<'a> Drop for Editor<'a> {
     fn drop(&mut self) {
         self.terminal.leave_alternate_screen().expect("Failed to leave alternate screen");
