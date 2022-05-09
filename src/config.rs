19e71d05f7842ecfa22509864c34c727ff7db3b3
CarsonFoster <carsonwfoster@gmail.com>
2022-05-09 13:04:15 -0500

    Add parsing of modifier key events Add additional parse errors and messages

--- src/config.rs
+++ src/config.rs
@@ -17,7 +17,7 @@ use crate::context::*;
 use std::fmt;
 use std::fs::read_to_string;
 use std::collections::HashMap;
-use crossterm::event::KeyEvent;
+use crossterm::event::{KeyCode, KeyEvent, KeyModifiers};
 
 #[derive(Debug)]
 #[non_exhaustive]
@@ -31,6 +31,10 @@ pub enum ConfigParseError {
     MalformedBindTerm{ line: u16 },
     /// Unexpected unicode character in the `bind` term.
     UnicodeBoundaryErrorInBind{ line: u16 },
+    /// The key event term isn't formed correctly.
+    MalformedKeyEventTerm{ line: u16 },
+    /// Unexpected unicode character in the key event term.
+    UnicodeBoundaryErrorInKeyEvent{ line: u16 },
     /// IO error (e.g. cannot open the config file)
     IOError{ error: std::io::Error },
 }
@@ -41,6 +45,10 @@ impl ConfigParseError {
         match self {
             ConfigParseError::NoMatchingContext{ context, line } => format!("line {}: no matching context {} found", line, context),
             ConfigParseError::NotEnoughTerms{ line } => format!("line {}: not enough terms (expected at least 3)", line),
+            ConfigParseError::MalformedBindTerm{ line } => format!("line {}: incorrect syntax in bind term", line),
+            ConfigParseError::UnicodeBoundaryErrorInBind{ line } => format!("line {}: unexpected unicode character in bind term", line),
+            ConfigParseError::MalformedKeyEventTerm{ line } => format!("line {}: incorrect syntax in key event term", line),
+            ConfigParseError::UnicodeBoundaryErrorInKeyEvent{ line } => format!("line {}: unexpected unicode character in key event term", line),
             ConfigParseError::IOError{ error } => error.to_string(),
             _ => "unknown config parse error".to_owned(),
         }
@@ -63,6 +71,62 @@ impl From<std::io::Error> for ConfigParseError {
 }
 
 impl std::error::Error for ConfigParseError {}
+
+struct ConfigMap {
+    unprintable: HashMap<String, KeyEvent>,
+}
+
+impl ConfigMap {
+    pub const MAP: ConfigMap = Self::new();
+
+    fn new() -> Self {
+        let unprintable = HashMap::new();
+        let pairs = [("BS", KeyCode::Backspace), ("CR", KeyCode::Enter), ("Enter", KeyCode::Enter), ("Left", KeyCode::Left),
+                     ("Right", KeyCode::Right), ("Up", KeyCode::Up), ("Down", KeyCode::Down), ("Home", KeyCode::Home),
+                     ("End", KeyCode::End), ("PageUp", KeyCode::PageUp), ("PageDown", KeyCode::PageDown), ("Tab", KeyCode::Tab),
+                     ("Del", KeyCode::Delete), ("Ins", KeyCode::Insert)];
+        for (rep, code) in pairs {
+            unprintable.insert(rep.to_string(), KeyEvent::new(code, KeyModifiers::NONE));
+        }
+        for i in 1u8..=12 {
+            unprintable.insert(format!("F{}", i), KeyEvent::new(KeyCode::F(i), KeyModifiers::NONE));
+        }
+        ConfigMap{ unprintable }
+    }
+
+    pub fn query(&self, rep: &str) -> Option<KeyEvent> {
+        let head = rep.chars().next();
+        let middle = rep.get(1..rep.len() - 1);
+        if rep.len() == 1 && head.is_some() && head.unwrap().is_ascii_graphic() {
+            Some(KeyEvent::new(KeyCode::Char(head.unwrap().to_ascii_uppercase()), KeyModifiers::NONE))
+        } else if rep.len() > 2 && rep.get(0..1) == Some("<") && rep.get(rep.len() - 1..) == Some(">")
+            && middle.is_some() && self.unprintable.contains_key(middle.unwrap()) {
+            Some(self.unprintable[middle.unwrap()])
+        } else {
+            None
+        }
+    }
+
+    pub fn query_code(&self, rep: &str) -> Option<KeyCode> {
+        let head = rep.chars().next();
+        if rep.len() == 1 && head.is_some() && head.unwrap().is_ascii_graphic() {
+            Some(KeyCode::Char(head.unwrap().to_ascii_uppercase()))
+        } else if self.unprintable.contains_key(rep) {
+            Some(self.unprintable[rep].code)
+        } else {
+            None
+        }
+    }
+}
+
+#[derive(PartialEq)]
+enum State {
+   Start, C, A, S, C_, A_, S_, Accept, Reject
+}
+
+enum Transition {
+    C, A, S, Hyphen, Else
+}
 
 /// Struct that represents key press to context mapping.
 pub struct Config {
@@ -103,6 +167,53 @@ impl Config {
         }
         if let Some(old_context) = bind.get(5..bind.len() - 1) {
             // TODO: parse key_event into KeyEvent
+            let single_key = ConfigMap::MAP.query(key_event);
+            let key_event = if let Some(key) = single_key { key } else {
+                if key_event.get(0..1) != Some("<") || key_event.get(key_event.len() - 1..) != Some(">") {
+                    return Err(ConfigParseError::MalformedKeyEventTerm{ line: line_no });
+                }
+                let mut modifiers = KeyModifiers::empty();
+                if let Some(key_event) = key_event.get(1..key_event.len() - 1) {
+                    let mut state = State::Start;
+                    let transitions = [[State::C, State::Reject, State::Reject, State::Reject, State::Accept, State::Accept, State::Accept],
+                                       [State::A, State::Reject, State::Reject, State::Reject, State::A, State::Accept, State::A],
+                                       [State::S, State::Reject, State::Reject, State::Reject, State::S, State::Accept, State::Accept],
+                                       [State::Reject, State::C_, State::A_, State::S_, State::Accept, State::Accept, State::Accept],
+                                       [State::Reject, State::Reject, State::Reject, State::Reject, State::Accept, State::Accept, State::Accept]];
+                    let mut key = String::with_capacity(8);
+                    for ch in key_event.chars() {
+                        let transition = match ch {
+                            'C' | 'c' => Transition::C,
+                            'A' | 'a' => Transition::A,
+                            'S' | 's' => Transition::S,
+                            '-'       => Transition::Hyphen,
+                            _         => Transition::Else,
+                        };
+                        state = transitions[transition as usize][state as usize];
+                        if state == State::Reject {
+                            return Err(ConfigParseError::MalformedKeyEventTerm{ line: line_no });
+                        } else if state == State::Accept {
+                            if key.len() == 8 {
+                                return Err(ConfigParseError::MalformedKeyEventTerm{ line: line_no });
+                            }
+                            key.push(ch); 
+                        }
+                        modifiers.insert(match state {
+                            State::C => KeyModifiers::CONTROL,
+                            State::A => KeyModifiers::ALT,
+                            State::S => KeyModifiers::SHIFT,
+                            _        => KeyModifiers::NONE
+                        });
+                    }
+                    let code = match ConfigMap::MAP.query_code(&key) {
+                        Some(c) => c,
+                        None => return Err(ConfigParseError::MalformedKeyEventTerm{ line: line_no })
+                    };
+                    KeyEvent::new(code, modifiers)
+                } else {
+                    return Err(ConfigParseError::UnicodeBoundaryErrorInKeyEvent{ line: line_no });
+                }
+            };
             // TODO: get args
            
             if let Some(factory) = context(new_context, args) {
