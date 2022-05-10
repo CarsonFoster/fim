87501737eaa74beae725a47d92f79ffc9d772bc6
CarsonFoster <carsonwfoster@gmail.com>
2022-05-10 17:00:32 -0500

    Add test for ConfigMap

--- src/config.rs
+++ src/config.rs
@@ -82,7 +82,7 @@ impl ConfigMap {
         let pairs = [("BS", KeyCode::Backspace), ("CR", KeyCode::Enter), ("Enter", KeyCode::Enter), ("Left", KeyCode::Left),
                      ("Right", KeyCode::Right), ("Up", KeyCode::Up), ("Down", KeyCode::Down), ("Home", KeyCode::Home),
                      ("End", KeyCode::End), ("PageUp", KeyCode::PageUp), ("PageDown", KeyCode::PageDown), ("Tab", KeyCode::Tab),
-                     ("Del", KeyCode::Delete), ("Ins", KeyCode::Insert)];
+                     ("Del", KeyCode::Delete), ("Ins", KeyCode::Insert), ("Space", KeyCode::Char(' ')), ("Esc", KeyCode::Esc)];
         for (rep, code) in pairs {
             unprintable.insert(rep.to_string(), KeyEvent::new(code, KeyModifiers::NONE));
         }
@@ -119,6 +119,71 @@ impl ConfigMap {
 
 lazy_static! {
     static ref MAP: ConfigMap = ConfigMap::new();
+}
+
+#[test]
+fn test_config_map() {
+    let ascii_graphics = "`1234567890-=~!@#$%^&*()_+qwertyuiop[]QWERTYUIOP{}asdfghjkl;'\\ASDFGHJKL:\"|zxcvbnm,./ZXCVBNM<>?";
+    for ch in ascii_graphics.chars() {
+        assert_eq!(MAP.query(&ch.to_string()), Some(KeyEvent::new(KeyCode::Char(ch.to_ascii_uppercase()), KeyModifiers::NONE)));
+        assert_eq!(MAP.query_code(&ch.to_string()), Some(KeyCode::Char(ch.to_ascii_uppercase())));
+    }
+    assert_eq!(MAP.query("<Tab>"), Some(KeyEvent::new(KeyCode::Tab, KeyModifiers::NONE)));
+    assert_eq!(MAP.query_code("Tab"), Some(KeyCode::Tab));
+    assert_eq!(MAP.query("<CR>"), Some(KeyEvent::new(KeyCode::Enter, KeyModifiers::NONE)));
+    assert_eq!(MAP.query_code("CR"), Some(KeyCode::Enter));
+    assert_eq!(MAP.query("<F1>"), Some(KeyEvent::new(KeyCode::F(1), KeyModifiers::NONE)));
+    assert_eq!(MAP.query_code("F1"), Some(KeyCode::F(1)));
+    assert_eq!(MAP.query("<F2>"), Some(KeyEvent::new(KeyCode::F(2), KeyModifiers::NONE)));
+    assert_eq!(MAP.query_code("F2"), Some(KeyCode::F(2)));
+    assert_eq!(MAP.query("<F3>"), Some(KeyEvent::new(KeyCode::F(3), KeyModifiers::NONE)));
+    assert_eq!(MAP.query_code("F3"), Some(KeyCode::F(3)));
+    assert_eq!(MAP.query("<F4>"), Some(KeyEvent::new(KeyCode::F(4), KeyModifiers::NONE)));
+    assert_eq!(MAP.query_code("F4"), Some(KeyCode::F(4)));
+    assert_eq!(MAP.query("<F5>"), Some(KeyEvent::new(KeyCode::F(5), KeyModifiers::NONE)));
+    assert_eq!(MAP.query_code("F5"), Some(KeyCode::F(5)));
+    assert_eq!(MAP.query("<F6>"), Some(KeyEvent::new(KeyCode::F(6), KeyModifiers::NONE)));
+    assert_eq!(MAP.query_code("F6"), Some(KeyCode::F(6)));
+    assert_eq!(MAP.query("<F7>"), Some(KeyEvent::new(KeyCode::F(7), KeyModifiers::NONE)));
+    assert_eq!(MAP.query_code("F7"), Some(KeyCode::F(7)));
+    assert_eq!(MAP.query("<F8>"), Some(KeyEvent::new(KeyCode::F(8), KeyModifiers::NONE)));
+    assert_eq!(MAP.query_code("F8"), Some(KeyCode::F(8)));
+    assert_eq!(MAP.query("<F9>"), Some(KeyEvent::new(KeyCode::F(9), KeyModifiers::NONE)));
+    assert_eq!(MAP.query_code("F9"), Some(KeyCode::F(9)));
+    assert_eq!(MAP.query("<F10>"), Some(KeyEvent::new(KeyCode::F(10), KeyModifiers::NONE)));
+    assert_eq!(MAP.query_code("F10"), Some(KeyCode::F(10)));
+    assert_eq!(MAP.query("<F11>"), Some(KeyEvent::new(KeyCode::F(11), KeyModifiers::NONE)));
+    assert_eq!(MAP.query_code("F11"), Some(KeyCode::F(11)));
+    assert_eq!(MAP.query("<F12>"), Some(KeyEvent::new(KeyCode::F(12), KeyModifiers::NONE)));
+    assert_eq!(MAP.query_code("F12"), Some(KeyCode::F(12)));
+    assert_eq!(MAP.query("<Ins>"), Some(KeyEvent::new(KeyCode::Insert, KeyModifiers::NONE)));
+    assert_eq!(MAP.query_code("Ins"), Some(KeyCode::Insert));
+    assert_eq!(MAP.query("<Del>"), Some(KeyEvent::new(KeyCode::Delete, KeyModifiers::NONE)));
+    assert_eq!(MAP.query_code("Del"), Some(KeyCode::Delete));
+    assert_eq!(MAP.query("<Home>"), Some(KeyEvent::new(KeyCode::Home, KeyModifiers::NONE)));
+    assert_eq!(MAP.query_code("Home"), Some(KeyCode::Home));
+    assert_eq!(MAP.query("<End>"), Some(KeyEvent::new(KeyCode::End, KeyModifiers::NONE)));
+    assert_eq!(MAP.query_code("End"), Some(KeyCode::End));
+    assert_eq!(MAP.query("<PageUp>"), Some(KeyEvent::new(KeyCode::PageUp, KeyModifiers::NONE)));
+    assert_eq!(MAP.query_code("PageUp"), Some(KeyCode::PageUp));
+    assert_eq!(MAP.query("<PageDown>"), Some(KeyEvent::new(KeyCode::PageDown, KeyModifiers::NONE)));
+    assert_eq!(MAP.query_code("PageDown"), Some(KeyCode::PageDown));
+    assert_eq!(MAP.query("<Left>"), Some(KeyEvent::new(KeyCode::Left, KeyModifiers::NONE)));
+    assert_eq!(MAP.query_code("Left"), Some(KeyCode::Left));
+    assert_eq!(MAP.query("<Right>"), Some(KeyEvent::new(KeyCode::Right, KeyModifiers::NONE)));
+    assert_eq!(MAP.query_code("Right"), Some(KeyCode::Right));
+    assert_eq!(MAP.query("<Up>"), Some(KeyEvent::new(KeyCode::Up, KeyModifiers::NONE)));
+    assert_eq!(MAP.query_code("Up"), Some(KeyCode::Up));
+    assert_eq!(MAP.query("<Down>"), Some(KeyEvent::new(KeyCode::Down, KeyModifiers::NONE)));
+    assert_eq!(MAP.query_code("Down"), Some(KeyCode::Down));
+    assert_eq!(MAP.query("<Enter>"), Some(KeyEvent::new(KeyCode::Enter, KeyModifiers::NONE)));
+    assert_eq!(MAP.query_code("Enter"), Some(KeyCode::Enter));
+    assert_eq!(MAP.query("<Space>"), Some(KeyEvent::new(KeyCode::Char(' '), KeyModifiers::NONE)));
+    assert_eq!(MAP.query_code("Space"), Some(KeyCode::Char(' ')));
+    assert_eq!(MAP.query("<BS>"), Some(KeyEvent::new(KeyCode::Backspace, KeyModifiers::NONE)));
+    assert_eq!(MAP.query_code("BS"), Some(KeyCode::Backspace));
+    assert_eq!(MAP.query("<Esc>"), Some(KeyEvent::new(KeyCode::Esc, KeyModifiers::NONE)));
+    assert_eq!(MAP.query_code("Esc"), Some(KeyCode::Esc));
 }
 
 #[derive(Clone, Copy, PartialEq)]
