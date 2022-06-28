0109c72e7ed91072c788939259f79819e2fdbd1e
CarsonFoster <carsonwfoster@gmail.com>
2022-06-28 14:11:13 -0500

    Add From traits to number options for free conversions

--- option_number/src/lib.rs
+++ option_number/src/lib.rs
@@ -16,7 +16,7 @@ fn impl_option_number(ast: &DeriveInput) -> TokenStream {
             if fields.unnamed.len() == 1 {
                 let field = fields.unnamed.first().unwrap(); 
                 match &field.ty {
-                    Type::Path(type_path) if type_path.path.is_ident("i32") => return gen_from_str(name),
+                    Type::Path(type_path) if type_path.path.is_ident("i32") => return gen_impls(name),
                     _ => ()
                 }
             }
@@ -25,13 +25,25 @@ fn impl_option_number(ast: &DeriveInput) -> TokenStream {
     panic!("the OptionNumber derive macro only works on structs with one unnamed, i32 field");
 }
 
-fn gen_from_str(name: &Ident) -> TokenStream {
+fn gen_impls(name: &Ident) -> TokenStream {
     let gen = quote! {
         impl ::std::str::FromStr for #name {
             type Err = ::std::num::ParseIntError;
             fn from_str(s: &str) -> ::std::result::Result<Self, Self::Err> {
                 let i: i32 = s.parse()?;
                 Ok(#name(i))
+            }
+        }
+
+        impl ::std::convert::From<#name> for i32 {
+            fn from(t: #name) -> Self {
+                t.0
+            }
+        }
+
+        impl ::std::convert::From<i32> for #name {
+            fn from(t: i32) -> Self {
+                #name(t)
             }
         }
     };
