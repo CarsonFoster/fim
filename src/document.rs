d3fe70a6585fb7c4e73fcd8ea4e5392ad09b7ebf
CarsonFoster <carsonwfoster@gmail.com>
2022-07-13 20:35:12 -0500

    Remove old Document code Add Buffer impl (not finished)

--- src/document.rs
+++ src/document.rs
@@ -1,22 +1,44 @@
 //! A module for handling the content of files ('documents').
 
+use std::fs::File;
 use std::io::Error;
+use std::ops::{Bound, Index, RangeBounds};
 use std::path::PathBuf;
 use std::slice::{Iter, SliceIndex};
 use unicode_segmentation::UnicodeSegmentation;
 
-/// Struct that represents a line of text.
-#[derive(Default)]
-pub struct Line {
-    /// The content of the line.
-    pub text: String,
-    /// The number of graphemes in the line.
-    pub graphemes: usize,
+struct Buffer {
+    buf: String,
+    graphemes: Vec<usize>,
 }
 
-impl From<&str> for Line {
-    fn from(text: &str) -> Self {
-        Line{ text: text.to_string(), graphemes: text.graphemes(true).count() }
+impl Buffer {
+    pub fn new(buf: String) -> Self {
+        let mut graphemes = Vec::new();
+        for (idx, g) in buf.as_str().grapheme_indices(true) {
+            graphemes.push(idx);
+        }
+        graphemes.push(buf.len()); // past-the-end grapheme
+        Buffer{ buf, graphemes }
+    }
+}
+
+impl<T> Index<T> for Buffer
+where T: RangeBounds<usize> {
+    type Output = str;
+
+    fn index(&self, idx: T) -> &Self::Output {
+        let begin = match idx.start_bound() {
+            Bound::Included(i) => self.graphemes[*i],
+            Bound::Excluded(i) => self.graphemes[*i + 1],
+            Bound::Unbounded => 0
+        };
+        let end = match idx.end_bound() {
+            Bound::Included(i) => self.graphemes[*i + 1],
+            Bound::Excluded(i) => self.graphemes[*i],
+            Bound::Unbounded => self.buf.len()
+        };
+        &self.buf[begin..end]
     }
 }
 
@@ -25,7 +47,7 @@ pub struct Document {
     #[doc(hidden)]
     filename: Option<PathBuf>,
     #[doc(hidden)]
-    lines: Vec<Line>,
+    buffers: /* TODO */
 }
 
 // TODO: transform incoming tabs according to options??
@@ -33,60 +55,22 @@ pub struct Document {
 impl Document {
     /// Create a new Document from a file.
     pub fn new(filename: PathBuf) -> Result<Self, Error> {
-        let text = std::fs::read_to_string(&filename)?;
-        Ok(Document{ filename: Some(filename), lines: Self::vec_from_str(&text) })
+        let fin = File::open(filename)?;
     }
 
-    /// Return the number of lines in the document.
-    pub fn num_lines(&self) -> usize {
-        self.lines.len()
-    }
-
-    /// Get a line at the given (zero-based) index.
-    pub fn line(&self, idx: usize) -> Option<&Line> {
-        self.lines.get(idx)
-    }
-
-    /// Get a mutable line at the given (zero-based) index.
-    pub fn line_mut(&mut self, idx: usize) -> Option<&mut Line> {
-        self.lines.get_mut(idx)
-    }
-
-    /// Retrieve an iterator into the lines of this document, starting from the given (zero-based)
-    /// index, inclusive.
-    pub fn iter_from(&self, line_idx: usize) -> Option<Iter<Line>> {
-        self.lines.get(line_idx..).map(|s| s.iter())
-    }
-
-    /// Get a unicode character with the given (zero-based) range from the given line.
-    pub fn unicode_char<I>(&self, line_idx: usize, char_range: I) -> Option<&<I as SliceIndex<str>>::Output>
-    where I: SliceIndex<str>
-    {
-        self.lines.get(line_idx).map(|l| l.text.get(char_range)).flatten()
-    }
-
-    /// Get a ASCII character with the given (zero-based) range from the given line.
-    pub fn ascii_char(&self, line_idx: usize, char_idx: usize) -> Option<char> {
-        self.lines.get(line_idx).map(|l| l.text.get(char_idx .. char_idx + 1)
-            .map(|s| s.chars().next()).flatten()).flatten()
-    }
-
-    fn vec_from_str(text: &str) -> Vec<Line> {
-        text.lines().map(|s| Line::from(s)).collect()
-    }
 }
 
-impl From<&str> for Document {
-    fn from(internal_doc: &str) -> Self {
-        Document{ filename: None, lines: Self::vec_from_str(internal_doc) }
+impl<T> From<T> for Document
+where T: AsRef<str>{
+    fn from(internal_doc: T) -> Self {
     }
 }
 
 impl<'a> IntoIterator for &'a Document {
-    type Item = &'a Line;
-    type IntoIter = Iter<'a, Line>;
+    type Item = /* TODO &'a item */;
+    type IntoIter = Iter<'a, /* TODO item */>;
 
     fn into_iter(self) -> Self::IntoIter {
-        (&self.lines).iter()
+        (/* TODO */).iter()
     }
 }
