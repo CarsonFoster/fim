50d8df5a0cb577e5df36097d5dec8d8ac5fea4b9
CarsonFoster <carsonwfoster@gmail.com>
2022-07-28 14:07:47 -0500

    Add first test of buffer

--- document/src/lib.rs
+++ document/src/lib.rs
@@ -2,13 +2,32 @@
 
 pub mod buffer;
 
-use crate::buffer::Buffer;
 
 #[cfg(test)]
 mod tests {
+    use crate::buffer::Buffer;
+
+    fn construct_buffer(ascii: bool) -> (String, Buffer) {
+        let mut string = String::new();
+        if ascii {
+            for _ in 0..500 {
+                for c in 0u8..127u8 {
+                    string.push(c as char);
+                }
+            }
+        } else {
+
+        }
+
+        let clone = string.clone();
+        (string, Buffer::new(clone))
+    }
+
     #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+    fn single_ascii() {
+        let (string, mut buf) = construct_buffer(true);
+        for i in 0..string.len() {
+            assert_eq!(&string[i..i + 1], buf.get((i as u16)..((i + 1) as u16)));
+        }
     }
 }
